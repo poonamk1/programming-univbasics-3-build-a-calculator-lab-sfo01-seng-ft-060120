4dd3ef729663b6afae6a59aac1b402c53d88031a
punam kwatra <punam2005@hotmail.com>
2020-04-26 22:30:56 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,9 +1,12 @@
-def addition(num1, num2)
-
+def addition (num1, num2)
+ sum = num2 + num1
+ return sum
+ 
 end
 
 def subtraction(num1, num2)
-
+sum = num2 - num1
+return sum
 end
 
 def division(num1, num2)
